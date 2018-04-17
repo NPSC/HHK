a85e98b78c363d17c2033254897ef3c5b69fc7fd
Will Ireland <will@wireland.me>
2018-04-17 15:07:33 -0500

    add patch

--- patch/patchSQL.sql
+++ patch/patchSQL.sql
@@ -1,6 +1,5 @@
 
+INSERT INTO `sys_config` (`Key`, `Value`, `Type`, `Category`, `Description`)
+VALUES
+	('VisitFeeDelayDays', '5', 'i', 'h', 'Number of days before cleaning fee is charged');
 
-REPLACE INTO `gen_lookups` (`Table_Name`, `Code`, `Description`, `Substitute`, `Type`, `Order`) VALUES
-('Editable_Forms', '../conf/agreement.txt', 'Registration Agreement','js/rte-agreement.json','',0),
-('Editable_Forms', '../conf/confirmation.txt', 'Confirmation Form','js/rte-confirmation.json','',0),
-('Editable_Forms', '../conf/survey.txt', 'Survey Form','js/rte-survey.json','',0);
