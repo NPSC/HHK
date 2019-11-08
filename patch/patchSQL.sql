797209d204c0d682d720b2de18b1efea0d8afff4
Will Ireland <will@wireland.me>
2019-11-08 12:15:44 -0600

    add UseDocumentUpload to sys_config

--- patch/patchSQL.sql
+++ patch/patchSQL.sql
@@ -1,2 +1,4 @@
 
 UPDATE `sys_config` SET `Category`='fg' WHERE `Key`='BatchSettlementHour';
+
+INSERT IGNORE INTO `sys_config` (`Key`, `Value`, `Type`, `Category`, `Header`, `Description`, `GenLookup`) VALUES('UseDocumentUpload', 'false', 'b', 'h', '', 'Enable Document Uploads', '');
