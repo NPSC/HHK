4921057f9656b29376adfbc150ac827c8964c891
Eric Crane <ecrane@nonprofitsoftwarecorp.org>
2020-09-03 09:56:33 -0500

    Update build #

--- house/homeIncludes.php
+++ house/homeIncludes.php
@@ -8,7 +8,7 @@
  * @link      https://github.com/NPSC/HHK
  */
 
-define('JSV', '?eG=92');
+define('JSV', '');
 
 define('HOUSE_CSS', "<link href='css/house.css" . JSV . "' rel='stylesheet' type='text/css' />");
 
