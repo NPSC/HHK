6676054ca1a1d246f7dcba733d006366d3827578
Will Ireland <will15@jwireland.com>
2021-06-11 15:04:18 -0500

    Add Google API keys to session

--- classes/sec/Login.php
+++ classes/sec/Login.php
@@ -84,6 +84,9 @@ class Login {
         	$ssn->rolecode = WebRole::Guest;
         }
         
+        //get google API keys
+        SysConfig::getCategory($dbh, $ssn, "'ga'", webInit::SYS_CONFIG);
+        
         return $dbh;
     }
 
