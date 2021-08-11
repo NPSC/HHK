abb815270f39995cc54e0e058072de7393952b6a
Will Ireland <will15@jwireland.com>
2021-08-11 12:47:22 -0500

    Fix required hospital name

--- house/js/formBuilder.js
+++ house/js/formBuilder.js
@@ -42,8 +42,7 @@
   				'patient.firstName',
   				'patient.lastName',
   				'checkindate',
-  				'checkoutdate',
-  				'hospital.name',
+  				'hospital.idHospital',
   				'submit'
   			],
             inputSets: [
