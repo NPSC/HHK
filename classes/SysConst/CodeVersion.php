2ee5aca3461dfa3b1bf60519fb73905e1b46c561
Will Ireland <will15@jwireland.com>
2022-02-28 11:46:44 -0600

    Build 227

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '226';
+    const BUILD = '227';
     const VERSION = '3.18';
     const PATCH = '1';
     const GIT_Id = 'dev';
