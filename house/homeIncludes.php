b5dbb8382b4d8277d8c5878e8b38d1cd3f6ef4f1
Will Ireland <will15@jwireland.com>
2022-05-05 11:14:11 -0500

    Build 231

--- house/homeIncludes.php
+++ house/homeIncludes.php
@@ -8,7 +8,7 @@
  * @link      https://github.com/NPSC/HHK
  */
 
-define('JSV', '?v=24');
+define('JSV', '?v=25');
 
 define('HOUSE_CSS', "<link href='css/house.css" . JSV . "' rel='stylesheet' type='text/css' />");
 
