a2e565fb6df0c95265ebbcf8f87ff63e391389d4
Will Ireland <will15@jwireland.com>
2022-05-18 15:02:34 -0500

    Prepare build 236

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '235';
+    const BUILD = '236';
     const VERSION = '3.19';
     const PATCH = '0';
     const GIT_Id = 'dev';
