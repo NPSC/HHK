48a577f6c1fc9768ec2e3d751f3177dada32234b
Eric Crane <ecrane@nonprofitsoftwarecorp.org>
2022-11-28 10:43:00 -0600

    Update CodeVersion.php

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,9 +11,9 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '248';
-    const VERSION = '3.19';
-    const PATCH = '3';
+    const BUILD = '249';
+    const VERSION = '3.20';
+    const PATCH = '0';
     const GIT_Id = 'dev';
     const REL_DATE = '';
 }
