cdfa3694b1146fcfc95729c6ae8551c6c8db68a0
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2022-11-28 11:07:33 -0600

    Minify build 3.20.0.249

--- house/homeIncludes.php
+++ house/homeIncludes.php
@@ -8,7 +8,7 @@
  * @link      https://github.com/NPSC/HHK
  */
 
-define('JSV', '?v=42');
+define('JSV', '?v=43');
 
 define('HOUSE_CSS', "<link href='ws_resc.php" . JSV . "&cmd=getCssVars' rel='stylesheet' type='text/css' /> <link href='css/house.css" . JSV . "' rel='stylesheet' type='text/css' />");
 define('ROOT_CSS', "<link href='../css/root.css" . JSV . "' rel='stylesheet' type='text/css' />");
