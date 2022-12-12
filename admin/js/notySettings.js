7b027055c1145efabb4869cfa236017dc550bd57
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2022-12-12 12:31:14 -0600

    Fix noty issues

--- admin/js/notySettings.js
+++ admin/js/notySettings.js
@@ -3,9 +3,14 @@ Noty.overrideDefaults({
     theme    : 'semanticui',
     timeout  : '4000',
     progressBar : true,
-    closeWith: ['click', 'button'],
+    closeWith: ['click','button'],
     animation: {
         open : 'animated bounceInDown',
         close: 'animated bounceOutUp'
     }
+});
+
+//Close banner messages when accessing navbar dropdowns
+$(document).on('show.bs.dropdown', '.navbar .dropdown', function(){
+	Noty.closeAll();
 });
--- house/js/notySettings.js
+++ house/js/notySettings.js
@@ -1,13 +1,16 @@
-
 Noty.overrideDefaults({
     layout   : 'top',
     theme    : 'semanticui',
     timeout  : '4000',
     progressBar : true,
-    closeWith: ['click'],
+    closeWith: ['click', 'button'],
     animation: {
         open : 'animated bounceInDown',
         close: 'animated bounceOutUp'
     }
 });
 
+//Close banner messages when accessing navbar dropdowns
+$(document).on('show.bs.dropdown', '.navbar .dropdown', function(){
+	Noty.closeAll();
+});
