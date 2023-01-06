d7a851bed9b40fcbf217f8ba9b6530c8bba826f2
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2023-01-06 11:01:43 -0600

    Revert "Fix emergency contact inconsistencies"
    This reverts commit e380bec8a2eb6395507d4ff6e82eecb0cdcca090.

--- classes/House/Reservation/Reservation.php
+++ classes/House/Reservation/Reservation.php
@@ -59,7 +59,7 @@ class Reservation {
             // Force new PSG, also implies new reservation
             $rData->setIdResv(0);
 
-            return new Reservation($rData, new ReservationRS(), new JoinNewFamily($dbh, $rData));
+            return new Reservation($rData, new ReservationRS(), new JoinNewFamily($dbh, $rData, $uS->EmergContactReserv));
         }
 
         // idResv < 0
@@ -67,7 +67,7 @@ class Reservation {
 
             if ($rData->getIdPsg() > 0) {
                 // Force New Resv for existing PSG
-                return new ActiveReservation($rData, new ReservationRS(), new Family($dbh, $rData));
+                return new ActiveReservation($rData, new ReservationRS(), new Family($dbh, $rData, $uS->EmergContactReserv));
 
             } else {
 
@@ -103,7 +103,7 @@ class Reservation {
 
 
         // idPsg = 0; idResv = 0;
-        return new Reservation($rData, new ReservationRS(), new Family($dbh, $rData));
+        return new Reservation($rData, new ReservationRS(), new Family($dbh, $rData, $uS->EmergContactReserv));
 
     }
 
@@ -151,7 +151,7 @@ WHERE r.idReservation = " . $rData->getIdResv());
             ->setResvStatusCode($rows[0]['Status']);
 
         if (Reservation_1::isActiveStatus($rRs->Status->getStoredVal())) {
-            return new ActiveReservation($rData, $rRs, new Family($dbh, $rData));
+            return new ActiveReservation($rData, $rRs, new Family($dbh, $rData, $uS->EmergContactReserv));
         }
 
         if ($rRs->Status->getStoredVal() == ReservationStatus::Staying) {
@@ -164,7 +164,7 @@ WHERE r.idReservation = " . $rData->getIdResv());
         }
 
         // Turned away, cancelled, etc.
-        return new StaticReservation($rData, $rRs, new Family($dbh, $rData));
+        return new StaticReservation($rData, $rRs, new Family($dbh, $rData, $uS->EmergContactReserv));
 
     }
 
