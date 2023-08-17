e6e42ad0eb3a106d5073e317c747c4ebf33d0f4f
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2023-08-17 14:14:40 -0500

    Work on Billing Agent Report

--- classes/House/Report/BillingAgentReport.php
+++ classes/House/Report/BillingAgentReport.php
@@ -4,8 +4,11 @@ namespace HHK\House\Report;
 
 use HHK\HTMLControls\HTMLContainer;
 use HHK\HTMLControls\HTMLInput;
+use HHK\HTMLControls\HTMLSelector;
+use HHK\HTMLControls\HTMLTable;
 use HHK\sec\Session;
 use HHK\sec\Labels;
+use HHK\SysConst\VolMemberType;
 
 /**
  * BillingAgentReport.php
@@ -27,6 +30,7 @@ use HHK\sec\Labels;
 class BillingAgentReport extends AbstractReport implements ReportInterface {
 
     public array $diags;
+    public array $billingAgents;
     public array $selectedBillingAgents;
 
 
@@ -37,28 +41,50 @@ class BillingAgentReport extends AbstractReport implements ReportInterface {
         $this->description = "This report shows all patients who stayed in the time period and was invoiced to a specific billing agent";
         $this->inputSetReportName = "billingAgent";
 
+        $this->billingAgents = $this->loadBillingAgents($dbh);
+        if (filter_has_var(INPUT_POST, 'selResvStatus')) {
+            $this->selectedBillingAgents = filter_input(INPUT_POST, 'selBillingAgents', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+        }else{
+            $this->selectedBillingAgents = [];
+        }
+
         parent::__construct($dbh, $this->inputSetReportName, $request);
+    }
+
+    protected function loadBillingAgents(\PDO $dbh){
+        $stmt = $dbh->query("SELECT n.idName, if(trim(n.Name_Full) != '', n.Name_Full, n.Company) as `Title`" .
+            " FROM name n join name_volunteer2 nv on n.idName = nv.idName and nv.Vol_Category = 'Vol_Type'  and nv.Vol_Code = '" . VolMemberType::BillingAgent . "' " .
+            " where n.Member_Status='a' and n.Record_Member = 1 order by n.Company");
+        return $stmt->fetchAll(\PDO::FETCH_NUM);
+    }
+
+    protected function getBillingAgentMarkup(){
+
+        $billingAgentSelector = HTMLSelector::generateMarkup(HTMLSelector::doOptionsMkup($this->billingAgents, $this->selectedBillingAgents), array('name' => 'selBillingAgents[]', 'size'=>(count($this->billingAgents) < 13 ? count($this->billingAgents) + 1 : '13'), 'multiple'=>'multiple'));
+    
+        $tbl = new HTMLTable();
+        $tr = '';
+    
+        $tbl->addHeaderTr(HTMLTable::makeTh("Billing Agent"));
+    
+        $tbl->addBodyTr($tr . HTMLTable::makeTd($billingAgentSelector, array('style'=>'vertical-align: top;')));
+    
+        return $tbl;
     }
 
     public function makeQuery(): void{
 
-        $arrivalCase = "CASE WHEN s.Span_Start_Date IS NOT NULL THEN s.Span_Start_Date
-        WHEN r.Actual_Arrival IS NOT NULL THEN r.Actual_Arrival
-        WHEN r.Expected_Arrival IS NOT NULL THEN r.Expected_Arrival ELSE NULL END";
+        $departureCase = "CASE WHEN v.Actual_Departure IS NOT NULL THEN v.Actual_Departure
+         WHEN v.Expected_Departure IS NOT NULL AND v.Expected_Departure > NOW() THEN v.Expected_Departure
+         WHEN v.Status = 'a' THEN ''
+         ELSE '' END";
 
-        $departureCase = "CASE WHEN s.Span_End_Date IS NOT NULL THEN s.Span_End_Date
-         WHEN s.Expected_Co_Date IS NOT NULL AND s.Expected_Co_Date > NOW() THEN s.Expected_Co_Date
-         WHEN r.Actual_Departure IS NOT NULL THEN r.Actual_Departure
-         WHEN s.Status = 'a' THEN ''
-         WHEN r.Expected_Departure IS NOT NULL THEN r.Expected_Departure ELSE '' END";
+        $whDepartureCase = "CASE WHEN v.Actual_Departure IS NOT NULL THEN v.Actual_Departure
+        WHEN v.Expected_Departure IS NOT NULL AND v.Expected_Departure > NOW() THEN v.Expected_Departure
+        WHEN v.Status = 'a' THEN ''
+        ELSE NOW() END";
 
-        $whDepartureCase = "CASE WHEN s.Span_End_Date IS NOT NULL THEN s.Span_End_Date
-         WHEN s.Expected_Co_Date IS NOT NULL AND s.Expected_Co_Date > NOW() THEN s.Expected_Co_Date
-         WHEN r.Actual_Departure IS NOT NULL THEN r.Actual_Departure
-         WHEN s.Status IS NOT NULL THEN NOW()
-         WHEN r.Expected_Departure IS NOT NULL THEN r.Expected_Departure ELSE NOW() END";
-
-        $whDates = $arrivalCase . " <= '" . $this->filter->getReportEnd() . "' and " . $whDepartureCase . " >= '" . $this->filter->getReportStart() . "' ";
+        $whDates =  "v.Arrival_Date <= '" . $this->filter->getReportEnd() . "' and " . $whDepartureCase . " >= '" . $this->filter->getReportStart() . "' ";
 
         $whBilling = "";
         if(count($this->selectedBillingAgents) > 0){
@@ -68,6 +94,7 @@ class BillingAgentReport extends AbstractReport implements ReportInterface {
 
         $this->query = "select
     CONCAT(v.idVisit, '-', v.Span) as idVisit,
+    ifnull(p.idName, '') as `pId`,
     ifnull(p.Name_Last, '') as Name_Last,
     ifnull(p.Name_First, '') as Name_First,
     concat(ifnull(pa.Address_1, ''), '', ifnull(pa.Address_2, ''))  as pAddr,
@@ -75,35 +102,49 @@ class BillingAgentReport extends AbstractReport implements ReportInterface {
     ifnull(pa.County, '') as pCounty,
     ifnull(pa.State_Province, '') as pState,
     ifnull(pa.Country_Code, '') as pCountry,
-    ifnull(pa.Postal_Code, '') as pZip,
-    " . $arrivalCase . " as `Arrival`,
+    ifnull(pa.Postal_Code, '') as `pZip`,
+    concat(ifnull(dc.Description, ''), ' ', ifnull(d.Description, '')) as `Diagnosis`,
+    ifnull(v.Arrival_Date, '') as `Arrival`,
     " . $departureCase . " as `Departure`,
-    DATEDIFF(ifnull(v.Actual_Departure, v.Expected_Departure), ifnull(v.Actual_Arrival, v.Expected_Arrival)) as `Nights`,
-    (DATEDIFF(ifnull(v.Actual_Departure, v.Expected_Departure), ifnull(v.Actual_Arrival, v.Expected_Arrival))+1) as `Days`,
-    ifnull(pgn.Name_First) as `pgFirst`,
-    ifnull(pgn.Name_Last) as `pgLast`,
+    DATEDIFF(ifnull(v.Actual_Departure, v.Expected_Departure), v.Arrival_Date) as `Nights`,
+    (DATEDIFF(ifnull(v.Actual_Departure, v.Expected_Departure), v.Arrival_Date)+1) as `Days`,
+    ifnull(pgn.Name_First, '') as `pgFirst`,
+    ifnull(pgn.Name_Last, '') as `pgLast`,
+    ifnull(vs.Description, '') as `Status_Title`,
+    if(trim(ba.Name_Full) != '', ba.Name_Full, ba.Company) as `Billed To`,
+    ifnull(invs.Description, '') as `Invoice_Status_Title`
 from
     visit v
         left join
     hospital_stay hs on v.idHospital_Stay = hs.idHospital_Stay
         left join
+    gen_lookups d on hs.Diagnosis = d.Code and d.`Table_Name` = 'Diagnosis'
+        left join
+    gen_lookups dc on d.Code = dc.Code and dc.Table_Name = 'diagnosis_category'
+        left join
     name p ON hs.idPatient = p.idName
         left join
     name_address pa ON p.idName = pa.idName and p.Preferred_Mail_Address = pa.Purpose
         left join
-    name pgn ON v.idPrimary_Guest = pgn.idName
+    name pgn ON v.idPrimaryGuest = pgn.idName
         left join
-    lookups g ON g.Category = 'ReservStatus'
-        and g.`Code` = r.`Status`
+    gen_lookups vs on vs.Table_Name = 'Visit_Status' and vs.Code = v.Status
+        join
+    invoice i on v.idVisit = i.Order_Number and v.Span = i.Suborder_Number
         left join
-    gen_lookups vs on vs.Table_Name = 'Visit_Status' and vs.Code = s.Status
+    gen_lookups invs on invs.Table_Name = 'Invoice_Status' and invs.Code = i.Status
+        left join
+    name ba on i.Sold_To_Id = ba.idName
 where " . $whDates . $whBilling . " order by v.idVisit";
     }
 
     public function makeFilterMkup():void{
         $this->filterMkup .= $this->filter->timePeriodMarkup()->generateMarkup();
+        $this->filterMkup .= $this->getBillingAgentMarkup()->generateMarkup();
         $this->filterMkup .= $this->getColSelectorMkup();
     }
+
+    
 
     public function makeFilterOptsMkup():void{
 
@@ -114,7 +155,7 @@ where " . $whDates . $whBilling . " order by v.idVisit";
         $uS = Session::getInstance();
         
         $cFields[] = array("Visit ID", 'idVisit', 'checked', '', 'string', '20');
-        $cFields[] = array("Patient ID", 'Name_First', 'checked', '', 'string', '20');
+        $cFields[] = array("Patient ID", 'pId', 'checked', '', 'string', '20');
         $cFields[] = array("Patient First", 'Name_First', 'checked', '', 'string', '20');
         $cFields[] = array("Patient Last", 'Name_Last', 'checked', '', 'string', '20');
 
@@ -133,13 +174,13 @@ where " . $whDates . $whBilling . " order by v.idVisit";
         $cFields[] = array($pTitles, $pFields, '', '', 'string', '15', array());
 
         $cFields[] = array("Diagnosis", 'Diagnosis', '', '', 'string', '20');
-        $cFields[] = array("Diagnosis Category", 'DiagnosisCategory', '', '', 'string', '20');
         $cFields[] = array("Arrive", 'Arrival', 'checked', '', 'MM/DD/YYYY', '15', array(), 'date');
         $cFields[] = array("Depart", 'Departure', 'checked', '', 'MM/DD/YYYY', '15', array(), 'date');
         $cFields[] = array("Primary Guest First", 'pgFirst', '', '', 'string', '20');
         $cFields[] = array("Parimary Guest Last", 'pgLast', '', '', 'string', '20');
-        $cFields[] = array("Status", 'Status_Title', 'checked', '', 'string', '15');
-        $cFields[] = array("Payment Status", 'Payment_Status_Title', 'checked', '', 'string', '15');
+        $cFields[] = array("Visit Status", 'Status_Title', 'checked', '', 'string', '15');
+        $cFields[] = array("Billed To", 'Billed To', '', '', 'string', '20');
+        $cFields[] = array("Invoice Status", 'Invoice_Status_Title', 'checked', '', 'string', '15');
 
 
         return $cFields;
@@ -160,7 +201,7 @@ where " . $whDates . $whBilling . " order by v.idVisit";
         $uS = Session::getInstance();
 
         foreach($this->resultSet as $k=>$r) {
-            $this->resultSet[$k]['Name_Last'] = HTMLContainer::generateMarkup('a', $r['Name_Last'], array('href'=>$uS->resourceURL . 'house/GuestEdit.php?id=' . $r['idGuest'] . '&psg=' . $r['idPsg']));
+            //$this->resultSet[$k]['Name_Last'] = HTMLContainer::generateMarkup('a', $r['Name_Last'], array('href'=>$uS->resourceURL . 'house/GuestEdit.php?id=' . $r['idGuest'] . '&psg=' . $r['idPsg']));
         }
 
         return parent::generateMarkup($outputType);
