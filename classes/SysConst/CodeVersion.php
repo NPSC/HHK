93403ef06f43f3c44ed88068beff1fe81b53054e
Eric Crane <ecrane@nonprofitsoftwarecorp.org>
2023-09-18 09:36:35 -0500

    upped code version and minifies

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -5,13 +5,13 @@ namespace HHK\SysConst;
  * CodeVersion.php
  *
  * @author    Eric K. Crane <ecrane@nonprofitsoftwarecorp.org>
- * @copyright 2010-2020 <nonprofitsoftwarecorp.org>
+ * @copyright 2010-2023 <nonprofitsoftwarecorp.org>
  * @license   MIT
  * @link      https://github.com/NPSC/HHK
  */
 
 class CodeVersion {
-    const BUILD = '270';
+    const BUILD = '271';
     const VERSION = '3.21';
     const PATCH = '2';
     const GIT_Id = 'dev';
