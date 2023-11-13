0064f621f7db834c67fd7ba3d7c5fe0a5ed54a1a
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2023-11-13 12:33:44 -0600

    Set showRegEmptyFields default true

--- patch/patchSQL.sql
+++ patch/patchSQL.sql
@@ -88,4 +88,4 @@ ADD COLUMN IF NOT EXISTS `DistCalcType` VARCHAR(10) NULL DEFAULT NULL AFTER `Met
 INSERT IGNORE INTO `labels` (`Key`, `Value`, `Type`, `Category`) VALUES ('ExtraPayment', 'Extra Payment', 's', 'pc');
 
 -- add showRegEmptyFields toggle
-INSERT IGNORE INTO `sys_config` (`Key`, `Value`, `Type`, `Category`, `Description`, `Show`) VALUES ('showRegEmptyFields', 'false', 'b', 'h', 'On Registrations, show empty fields', '1');
+INSERT IGNORE INTO `sys_config` (`Key`, `Value`, `Type`, `Category`, `Description`, `Show`) VALUES ('showRegEmptyFields', 'true', 'b', 'h', 'On Registrations, show empty fields', '1');
