0d12952383f2a5adcc8caeeb9c8b47f09f277045
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2023-11-27 12:05:13 -0600

    Update CodeVersion.php

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '279';
+    const BUILD = '280';
     const VERSION = '3.21';
     const PATCH = '2';
     const GIT_Id = 'dev';
