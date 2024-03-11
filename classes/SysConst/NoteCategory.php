1ca55ef24b76a81276b3d9ffef655943046f3187
Eric Crane <ecrane@nonprofitsoftwarecorp.org>
2024-03-11 13:19:13 -0500

    New checklist_item table

--- classes/SysConst/NoteCategory.php
+++ classes/SysConst/NoteCategory.php
@@ -17,4 +17,3 @@ class NoteCategory {
     const Visit = 'ncv';
     const Patient = 'ncp';
 }
-?>
--- sql/CreateAllTables.sql
+++ sql/CreateAllTables.sql
@@ -146,6 +146,22 @@ CREATE TABLE if not exists `cc_hosted_gateway` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 
+-- -----------------------------------------------------
+-- Table `checklist_item`
+-- -----------------------------------------------------
+CREATE TABLE `checklist_item` (
+  `idChecklist_item` int(11) NOT NULL AUTO_INCREMENT,
+  `GL_TableName` varchar(45) NOT NULL DEFAULT '',
+  `GL_Code` varchar(65) NOT NULL DEFAULT '',
+  `Status` varchar(5) NOT NULL DEFAULT '',
+  `Value` smallint(4) NOT NULL DEFAULT 0,
+  `Value_Date` datetime DEFAULT NULL,
+  `Updated_By` varchar(45) NOT NULL DEFAULT '',
+  `Last_Updated` datetime DEFAULT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`idChecklist_item`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
 
 -- -----------------------------------------------------
 -- Table `cleaning_log`
