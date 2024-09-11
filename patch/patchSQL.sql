04697371f0905a1f0e3230d333072a4980703859
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2024-09-11 10:58:53 -0500

    Add ADA to RibbonColor list

--- patch/patchSQL.sql
+++ patch/patchSQL.sql
@@ -173,3 +173,5 @@ ALTER TABLE `gen_lookups`
 ADD COLUMN IF NOT EXISTS `Attributes` JSON NOT NULL DEFAULT '{}' AFTER `Substitute`;
 
 INSERT IGNORE INTO `sys_config` (`Key`, `Value`, `Type`, `Category`, `Description`, `GenLookup`, `Show`) VALUES ('CurGuestDemogIcon','ADA','lu','h','Show this Demographic category on the Current Guests tab as an icon','Demographics',1);
+
+INSERT IGNORE INTO `gen_lookups` (`Table_Name`, `Code`, `Description`, `Order`) VALUES ('RibbonColors', 'ADA', 'ADA', '100');
