43a5fc6bb150c79d996905519201ce83fed38b1d
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2024-11-20 10:38:26 -0600

    Fix SimpleTexting Error handling + build 314

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '313';
+    const BUILD = '314';
     const VERSION = '3.24';
     const PATCH = '0';
     const GIT_Id = 'dev';
