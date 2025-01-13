74d7bf0e4d3abb5ae94d2e8447723db90ee37e1a
Will Ireland <will@wireland.me>
2025-01-13 10:23:11 -0600

    Build 315

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,9 +11,9 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '314';
+    const BUILD = '315';
     const VERSION = '3.24';
-    const PATCH = '0';
+    const PATCH = '1';
     const GIT_Id = 'dev';
     const REL_DATE = '';
 }
