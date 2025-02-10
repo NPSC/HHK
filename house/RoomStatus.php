f8dc8cc9ec894975b92c96d93b57bae64d0f4afe
Will Ireland <will@wireland.me>
2025-02-10 16:04:19 -0600

    Update RoomStatus.php

--- house/RoomStatus.php
+++ house/RoomStatus.php
@@ -762,34 +762,6 @@ if (isset($rescGroups[$uS->CalResourceGroupBy])) {
             <div class="my-1">
                 <h1><?php echo $wInit->pageHeading; ?></h1>
             </div>
-<!--
-            <div class="hhk-list-group">
-                <div class="list-group-item list-group-item-action active" aria-current="true">
-                    <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small>3 days ago</small>
-                    </div>
-                    <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    <small>And some small print.</small>
-                </div>
-                <div class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-body-secondary">3 days ago</small>
-                    </div>
-                    <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    <small class="text-body-secondary">And some muted small print.</small>
-                </div>
-                <div class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-body-secondary">3 days ago</small>
-                    </div>
-                    <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    <small class="text-body-secondary">And some muted small print.</small>
-                </div>
-            </div>
-        -->
 
             <form action="RoomStatus.php" method="post"  id="form1" name="form1">
                 <div id="mainTabs" style="font-size: .8em; display:none; width: 100%" class="hhk-tdbox hhk-mobile-tabs">
