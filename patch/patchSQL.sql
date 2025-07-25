a08777fab7b9476bd6516c571e124156641110a9
Will Ireland <will@wireland.me>
2025-07-25 09:00:28 -0500

    Update patchSQL.sql

--- patch/patchSQL.sql
+++ patch/patchSQL.sql
@@ -1,1 +1,2 @@
+UPDATE `sys_config` SET `Show` = 0 where `Key` in ("mode", "sId");
 INSERT IGNORE INTO `sys_config` (`Key`, `Value`, `Type`, `Category`, `Show`) VALUES ('useGLCodes', 'false', 'b', 'f', '0');
