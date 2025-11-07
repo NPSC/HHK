33cf702366bbd709d0e0dac81a85380afff804b5
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2025-11-07 09:14:07 -0600

    Cast occupancy percentages to float on occupancy api

--- classes/House/Report/DailyOccupancyReport.php
+++ classes/House/Report/DailyOccupancyReport.php
@@ -4,13 +4,15 @@ namespace HHK\House\Report;
 use DateTime;
 use HHK\Common;
 use HHK\HTMLControls\HTMLContainer;
-use HHK\SysConst\{VisitStatus,ReservationStatus};
+use HHK\SysConst\{VisitStatus, ReservationStatus};
 use HHK\sec\Session;
 use HHK\HTMLControls\HTMLTable;
 
-class DailyOccupancyReport extends AbstractReport implements ReportInterface {
+class DailyOccupancyReport extends AbstractReport implements ReportInterface
+{
 
-    public function __construct(\PDO $dbh, array $request = []){
+    public function __construct(\PDO $dbh, array $request = [])
+    {
         $uS = Session::getInstance();
 
         $this->reportTitle = $uS->siteName . ' Daily Occupancy Report';
@@ -20,23 +22,25 @@ class DailyOccupancyReport extends AbstractReport implements ReportInterface {
     }
 
     public function makeFilterMkup(): void
-    {}
+    {
+    }
 
-    public function makeSummaryMkup(): string {
+    public function makeSummaryMkup(): string
+    {
         $summaryData = $this->getMainSummaryData();
 
         $summaryTbl = new HTMLTable();
-        $summaryTbl->addBodyTr($summaryTbl->makeTd("Prepared at", array("class"=>"tdlabel")) . $summaryTbl->makeTd((new DateTime())->format("M j, Y h:i a")));
+        $summaryTbl->addBodyTr($summaryTbl->makeTd("Prepared at", array("class" => "tdlabel")) . $summaryTbl->makeTd((new DateTime())->format("M j, Y h:i a")));
 
-        foreach($summaryData[0] as $key=>$val){
-            if(in_array($key, ["Available Room Occupancy", "Total Room Occupancy"])){
+        foreach ($summaryData[0] as $key => $val) {
+            if (in_array($key, ["Available Room Occupancy", "Total Room Occupancy"])) {
                 $val .= "%";
             }
-            
-            $summaryTbl->addBodyTr($summaryTbl->makeTd($key . (isset($summaryData[1][$key]) ? '<span class="hhk-tooltip ui-icon ui-icon-help" title="' . $summaryData[1][$key] . '"></span>' : ''), array("class"=>"tdlabel")) . $summaryTbl->makeTd($val));
+
+            $summaryTbl->addBodyTr($summaryTbl->makeTd($key . (isset($summaryData[1][$key]) ? '<span class="hhk-tooltip ui-icon ui-icon-help" title="' . $summaryData[1][$key] . '"></span>' : ''), array("class" => "tdlabel")) . $summaryTbl->makeTd($val));
         }
 
-        return HTMLContainer::generateMarkup("div",$summaryTbl->generateMarkup(array("class"=>"mr-3 mb-3","style"=>"min-width: fit-content")), array("class"=>"hhk-flex hhk-flex-wrap hhk-visitdialog"));
+        return HTMLContainer::generateMarkup("div", $summaryTbl->generateMarkup(array("class" => "mr-3 mb-3", "style" => "min-width: fit-content")), array("class" => "hhk-flex hhk-flex-wrap hhk-visitdialog"));
 
     }
 
@@ -46,21 +50,23 @@ class DailyOccupancyReport extends AbstractReport implements ReportInterface {
     }
 
     public function makeQuery(): void
-    {}
+    {
+    }
 
-    public function getMainSummaryData(){
+    public function getMainSummaryData()
+    {
 
         $roomTypes = Common::readGenLookupsPDO($this->dbh, "Resource_Type");
-        $rmtroomTitle = (isset($roomTypes['rmtroom']['Description']) ? $roomTypes['rmtroom']['Description']: "Remote Room");
+        $rmtroomTitle = (isset($roomTypes['rmtroom']['Description']) ? $roomTypes['rmtroom']['Description'] : "Remote Room");
 
         $todayDT = new DateTime();
         $retiredRescSql = "(r.Retired_At is null or r.Retired_At > '" . $todayDT->format('Y-m-d') . "')";
 
         $resvStatuses = Common::readLookups($this->dbh, "reservStatus", "Code");
-        $resvStatusList = (isset($resvStatuses[ReservationStatus::Committed]['Title']) ? $resvStatuses[ReservationStatus::Committed]['Title'] . ", " : "") . 
-                (isset($resvStatuses[ReservationStatus::UnCommitted]['Title']) ? $resvStatuses[ReservationStatus::UnCommitted]['Title'] . ", " : "") . 
-                (isset($resvStatuses[ReservationStatus::Waitlist]['Title']) ? "and " . $resvStatuses[ReservationStatus::Waitlist]['Title'] : "");
-        
+        $resvStatusList = (isset($resvStatuses[ReservationStatus::Committed]['Title']) ? $resvStatuses[ReservationStatus::Committed]['Title'] . ", " : "") .
+            (isset($resvStatuses[ReservationStatus::UnCommitted]['Title']) ? $resvStatuses[ReservationStatus::UnCommitted]['Title'] . ", " : "") .
+            (isset($resvStatuses[ReservationStatus::Waitlist]['Title']) ? "and " . $resvStatuses[ReservationStatus::Waitlist]['Title'] : "");
+
         $query = "select
                     (select count(*) from resource r where r.Type = 'room' and $retiredRescSql ) as 'Total Rooms',
                     (select count(*) from resource r
