2b6128fb052ef88ffad34e3691467d8f6d9f0369
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2026-02-02 13:41:54 -0600

    builid343

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '342';
+    const BUILD = '343';
     const VERSION = '3.27';
     const PATCH = '0';
     const GIT_Id = 'dev';
