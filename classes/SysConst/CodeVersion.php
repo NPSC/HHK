73bc6cfdd1976bae73b26599aab63a37242c279d
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2026-02-16 11:36:19 -0600

    Fix sql name_search bug

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '344';
+    const BUILD = '345';
     const VERSION = '3.27';
     const PATCH = '0';
     const GIT_Id = 'dev';
