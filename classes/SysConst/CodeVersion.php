0e5a87d5a1df8b033381186591bfdf9ece2336ec
Will Ireland <wireland@nonprofitsoftwarecorp.org>
2026-06-10 11:07:39 -0500

    build 359

--- classes/SysConst/CodeVersion.php
+++ classes/SysConst/CodeVersion.php
@@ -11,7 +11,7 @@ namespace HHK\SysConst;
  */
 
 class CodeVersion {
-    const BUILD = '358-neonv2';
+    const BUILD = '359';
     const VERSION = '3.27';
     const PATCH = '1';
     const GIT_Id = 'dev';
